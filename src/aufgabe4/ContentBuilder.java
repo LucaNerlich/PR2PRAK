7980b4c6b7d2c7004fa9f16f90def1e147c0defc
LucaNerlich <luca.nerlich@googlemail.com>
2014-05-03 16:38:16 +0200

    aufgeraeumt

--- src/aufgabe4/ContentBuilder.java
+++ src/aufgabe4/ContentBuilder.java
@@ -1,17 +1,12 @@
 package aufgabe4;
 
-import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.*;
+
 
 /**
  * Erzeugt Inhalte fuer die Gridpane etc
