0159faba7c5d0633e116876d136319b82e339b24
LucaNerlich <luca.nerlich@googlemail.com>
2014-05-19 14:27:07 +0200

    alles aufgeräumt und tabelle init wieder in methoden

--- src/aufgabe4/GuiAnwendung.java
+++ src/aufgabe4/GuiAnwendung.java
@@ -24,27 +24,28 @@ import java.net.URL;
 
 public class GuiAnwendung extends Application {
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
+            launch(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			launch(args);
-		}
+    public TableView<Customers> customerTableView = new TableView();
+    public TableView<Products> productsTableView = new TableView();
 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// erstellt das Fenster mit Titel
-		ContentBuilder contBuilder = new ContentBuilder();
-		primaryStage.setTitle("Aufgabe4 - JavaFX");
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // erstellt das Fenster mit Titel
+        ContentBuilder contBuilder = new ContentBuilder();
+        primaryStage.setTitle("Aufgabe4 - JavaFX");
 
         BorderPane borderPane = new BorderPane();
 
         Scene scene = new Scene(borderPane);
+
         GridPane gridpane = new GridPane();
         VBox vBoxLeft = new VBox(8);
         VBox vBoxRight = new VBox(8);
@@ -54,33 +55,19 @@ public class GuiAnwendung extends Application {
         borderPane.setTop(createMenu());
         borderPane.setCenter(gridpane);
 
-
         // init Gridpane
-
-		gridpane.setPadding(new Insets(5)); // rand aussen
-		gridpane.setHgap(10);
-		gridpane.setVgap(10);
+        gridpane.setPadding(new Insets(5)); // rand aussen
+        gridpane.setHgap(10);
+        gridpane.setVgap(10);
 
         Text bgTitle = new Text("Aufgabe 4 - JavaFX");
         bgTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         gridpane.add(bgTitle, 0, 0, 2, 1);
 
-		ToggleButton tBtn1 = contBuilder.createToggleButton("Toggle Button");
-		ComboBox comboBox1 = contBuilder.createComboBox("Luca", "Fabian",
-				"Laura", "Daniel");
-		CheckBox ckBox1 = contBuilder.createCheckBox("el babo");
-		TextField tField = contBuilder.createTextField("");
-		Button btn = contBuilder.createButton("ADD PERSON");
+    /*
+        Button btn = contBuilder.createButton("test button");
         btn.setOnAction(new ButtonWithEventHandler());
-
-       final Button btn2 = contBuilder.createButton("Button2");
-       btn2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello from: " + actionEvent.getSource());
-                System.out.println(" - ID: " + btn2.getId());
-            }
-        });
+    */
 
         final Button closeButton = contBuilder.createButton("CLOSE");
         closeButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -92,42 +79,27 @@ public class GuiAnwendung extends Application {
 
         borderPane.setBottom(closeButton);
 
-		// erstellt die Flaeche des Fensters
-		// StackPane root = new StackPane();
-		// Group root = new Group();
-        // Scene scene = new Scene(new VBox(8), 600, 400);
-
-        // ((VBox) scene.getRoot()).getChildren().addAll(createMenu());
-        // ((VBox) scene.getRoot()).getChildren().addAll(gridpane);
-
-		Label label = new Label("Person_Table");
+        Label label = new Label("Person_Table");
         Label label2 = new Label("VBOX 8 LEFT");
         Label label3 = new Label("VBOX 8 RIGHT");
 
         vBoxLeft.getChildren().add(label2);
         vBoxRight.getChildren().add(label3);
 
-        label.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_MOVED, new MouseEventHandler());
-	// 	GridPane.setHalignment(label, HPos.CENTER);
+        // label.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_MOVED, new MouseEventHandler());
 
-        // Objekte an die Gridpane haengen
-		// root.getChildren().add(gridpane);
-		// fuege Button zur Gridpane hinzu
-		// gridpane.add(tBtn1, 0, 3);
-		// gridpane.add(comboBox1, 0, 2);
-		// gridpane.add(ckBox1, 1, 1);
-		// gridpane.add(tField, 2, 0);
         Label customersLabel = new Label("Customers");
         Label productsLabel = new Label("Products");
 
-        gridpane.add(customersLabel,1, 1);
-        gridpane.add(productsLabel,2, 1);
+        gridpane.add(customersLabel, 1, 1);
+        gridpane.add(productsLabel, 2, 1);
+        gridpane.add(label, 1, 2);
 
-		 gridpane.add(btn, 2, 2);
-        // gridpane.add(btn2, 2, 2);
-	    // root.getChildren().add(createMenu());
-		 // gridpane.add(createMenu(), 0, 0);
-         gridpane.add(label, 1, 2);
+        initCustomTable();
+        gridpane.add(customerTableView, 1, 2);
+
+        initProduktTable();
+        gridpane.add(productsTableView, 2, 2);
 
         Button addCustomer = contBuilder.createButton("Add customer");
         gridpane.add(addCustomer, 1, 3);
@@ -135,16 +107,18 @@ public class GuiAnwendung extends Application {
         addCustomer.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-               Stage stage = new Stage();
+                Stage stage = new Stage();
                 String vorname = Dialogs.showInputDialog(stage, "Bitte geben Sie den Vornamen ein:", "VORNAME", "");
                 String nachname = Dialogs.showInputDialog(stage, "Bitte geben Sie den Nachname ein:", "NACHNAME", "");
-                 try{
-                    int vInt = Integer.parseInt(vorname);
-                    int nInt = Integer.parseInt(nachname);
-                }
-                 //exception wird geworfen, wenn der input string ist.
-                catch(NumberFormatException e){
-                    Customers.addCustomer(vorname, nachname);
+                if (!vorname.equals("") && !nachname.equals("")) {
+                    try {
+                        int vInt = Integer.parseInt(vorname);
+                        int nInt = Integer.parseInt(nachname);
+                    }
+                    //exception wird geworfen, wenn der input string ist.
+                    catch (NumberFormatException e) {
+                        Customers.addCustomer(vorname, nachname);
+                    }
                 }
             }
         });
@@ -161,136 +135,60 @@ public class GuiAnwendung extends Application {
                 Stage stage = new Stage();
                 String item = Dialogs.showInputDialog(stage, "Bitte geben Sie die Bezeichnung des Produkts ein:", "BEZEICHNUNG", "");
                 String preis = Dialogs.showInputDialog(stage, "Bitte geben Sie den Preis ein:", "PREIS", "");
-                try{
-                int preisInt = Integer.parseInt(preis);
-                    Products.addProduct(item, preisInt);
-                }
-                catch(NumberFormatException e){
-                    System.out.println("Bitte geben sie einen Integer Wert ein!");
+                if (!item.equals("") && !preis.equals("")) {
+                    try {
+                        int preisInt = Integer.parseInt(preis);
+                        Products.addProduct(item, preisInt);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Bitte geben sie einen Integer Wert ein!");
+                    }
                 }
             }
         });
-
-
-
-
 
         Button placeOrder = contBuilder.createButton("Place order");
         gridpane.add(placeOrder, 1, 5);
 
-		//gridpane.add(createCustomers(), 1, 2);
-
-        // CUSTOMER TABLE VIEW  // CUSTOMER TABLE VIEW  // CUSTOMER TABLE VIEW  // CUSTOMER TABLE VIEW
-        final TableView<Customers> customerTableView = new TableView();
-
-        customerTableView.setPrefWidth(350);
-        customerTableView.setPrefHeight(300);
-        customerTableView.setItems(Customers.getCustomers());
-
-        // Setup the first column: vName
-        // 	TableColumn<Customers, String> vNameCol = new TableColumn<>("vName");  // intellij kann keine <>
-        TableColumn<Customers, String> vNameCol = new TableColumn("Vorname");
-        vNameCol.setEditable(true);
-        vNameCol.setCellValueFactory(new PropertyValueFactory<Customers, String>(
-                "vName"));
-        vNameCol.setPrefWidth(customerTableView.getPrefWidth() / 2);
-
-        // Setup the second column: nName
-        // TableColumn<Customers, String> nNameCol = new TableColumn<>("nName"); // intellij kann keine <>
-        TableColumn<Customers, String> nNameCol = new TableColumn("Nachname");
-        nNameCol.setCellValueFactory(new PropertyValueFactory<Customers, String>(
-                "nName"));
-        nNameCol.setPrefWidth(customerTableView.getPrefWidth() / 2);
-
-        // Assemble table from the columns
-        customerTableView.getColumns().add(vNameCol);
-        customerTableView.getColumns().add(nNameCol);
-
-        //LISTEN FOR CHANGES (eintrag markiert etc.)
-        customerTableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Customers>() {
-            @Override
-            public void changed(ObservableValue<? extends Customers> observableValue, Customers customers, Customers customers2) {
-            }
-        });
-
-        gridpane.add(customerTableView, 1, 2);
 
         Button removeCustomer = contBuilder.createButton("Remove selected customer");
         gridpane.add(removeCustomer, 1, 4);
 
-        // Entfernt selektiertes Item
+        // Entfernt selektiertes Item - Customer
         removeCustomer.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 int selectedIndex = customerTableView.getSelectionModel().getSelectedIndex();
-                try{
-                customerTableView.getItems().remove(selectedIndex);
-            }
-                catch(ArrayIndexOutOfBoundsException e){
+                try {
+                    customerTableView.getItems().remove(selectedIndex);
+                } catch (ArrayIndexOutOfBoundsException e) {
                     Stage stage = new Stage();
                     Dialogs.showInformationDialog(stage, "Bitte waehlen Sie zuerst einen Eintrag aus!", "Warning");
                 }
             }
         });
-        // CUSTOMER ENDE // CUSTOMER ENDE // CUSTOMER ENDE // CUSTOMER ENDE // CUSTOMER ENDE // CUSTOMER ENDE
-
-        // PRODUCT TABLE VIEW // PRODUCT TABLE VIEW // PRODUCT TABLE VIEW // PRODUCT TABLE VIEW // PRODUCT TABLE VIEW
-
-        final TableView<Products> productsTableView = new TableView();
-
-        productsTableView.setPrefWidth(350);
-        productsTableView.setPrefHeight(300);
-        productsTableView.setItems(Products.getProducts());
-
-        // Setup the first column: Name
-        // 	TableColumn<Products, String> NameCol = new TableColumn<>("Name"); // intellij kann keine <>
-        TableColumn<Products, String> NameCol = new TableColumn("Name");
-        NameCol.setEditable(true);
-        NameCol.setCellValueFactory(new PropertyValueFactory<Products, String>(
-                "Name"));
-        NameCol.setPrefWidth(productsTableView.getPrefWidth() / 2);
-
-        // Setup the second colum: Price
-        // TableColumn<Products, String> priceCol = new TableColumn<>("price");
-        TableColumn<Products, String> priceCol = new TableColumn("Price");
-        priceCol
-                .setCellValueFactory(new PropertyValueFactory<Products, String>(
-                        "Price"));
-        priceCol.setPrefWidth(productsTableView.getPrefWidth() / 2);
-
-        // Assemble table from the columns
-        productsTableView.getColumns().add(NameCol);
-        productsTableView.getColumns().add(priceCol);
-
-        gridpane.add(productsTableView, 2 ,2);
-        //gridpane.add(createProducts(),  2, 2);
 
         Button removeProducts = contBuilder.createButton("Remove selected product");
         gridpane.add(removeProducts, 2, 4);
 
-        // Entfernt selektiertes Item
+        // Entfernt selektiertes Item - Produkt
         removeProducts.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 int selectedIndex = productsTableView.getSelectionModel().getSelectedIndex();
-                try{
-                productsTableView.getItems().remove(selectedIndex);
-                }
-                catch(ArrayIndexOutOfBoundsException e){
+                try {
+                    productsTableView.getItems().remove(selectedIndex);
+                } catch (ArrayIndexOutOfBoundsException e) {
                     Stage stage = new Stage();
                     Dialogs.showInformationDialog(stage, "Bitte waehlen Sie zuerst einen Eintrag aus!", "Warning");
                 }
             }
         });
 
-        // PRODUKT ENDE // PRODUKT ENDE // PRODUKT ENDE // PRODUKT ENDE // PRODUKT ENDE // PRODUKT ENDE
-
         //css laden
         ObservableList<String> cssList = loadCss("stylesheet.css");
-        if (cssList != null){
+        if (cssList != null) {
             scene.getStylesheets().addAll(cssList);
-        }
-        else{
+        } else {
             System.err.println("Failed to load CSS file.");
         }
 
@@ -299,55 +197,17 @@ public class GuiAnwendung extends Application {
 
         // zeigt die Linien innerhalb der Gridpane
         // gridpane.setGridLinesVisible(true);
-		 primaryStage.show();		 
-		 
-		// primaryStage.setScene(new Scene(root, 1024, 720, Color.WHITE));
-		// primaryStage.show();
-	}
 
-	/**
-	 * returned eine Menubar
-	 * @return Menubar
-	 */
-	private MenuBar createMenu() {
-		MenuBar menuBar = new MenuBar();
-		Menu menu = new Menu("File");
-        final Menu menuClose = new Menu("Close");
-		menuBar.getMenus().add(menu);
-        menuBar.getMenus().add(menuClose);
-
-		menu.getItems().add(new MenuItem("New"));
-		menu.getItems().add(new MenuItem("Load"));
-		menu.getItems().add(new MenuItem("Save"));
-		menu.getItems().add(new MenuItem("Properties"));
-		menu.getItems().add(new MenuItem("Print"));
-
-		return menuBar;
-	}
-
-    private ObservableList<String> loadCss(String cssFileName){
-        ObservableList<String> cssStyle = FXCollections.observableArrayList();
-        URL url = getClass().getResource(cssFileName);
-        if (url == null){
-            return null;
-        }
-        cssStyle.addAll(url.toExternalForm());
-        return cssStyle;
+        primaryStage.show();
     }
-}
-/* ALTE TABELLEN - initalisierung durch methoden aufruf
-    Dann aber probleme mit dem Loeschen der Eintraege,
-    da nicht direkt draufzugegriffen werden kann.
 
- // CUSTOMER TABLE VIEW
-    private TableView createCustomers(){
-        final TableView<Customers> customerTableView = new TableView();
+    private void initCustomTable() {
         customerTableView.setPrefWidth(350);
         customerTableView.setPrefHeight(300);
         customerTableView.setItems(Customers.getCustomers());
 
         // Setup the first column: vName
-        // 	TableColumn<Customers, String> vNameCol = new TableColumn<>("vName");  // intellij kann keine <>
+        // TableColumn<Customers, String> vNameCol = new TableColumn<>("vName");  // intellij kann keine <>
         TableColumn<Customers, String> vNameCol = new TableColumn("Vorname");
         vNameCol.setEditable(true);
         vNameCol.setCellValueFactory(new PropertyValueFactory<Customers, String>(
@@ -361,25 +221,20 @@ public class GuiAnwendung extends Application {
                 "nName"));
         nNameCol.setPrefWidth(customerTableView.getPrefWidth() / 2);
 
-        // Assemble table from the columns
+        // Tabelle mit Spalten befuellen
         customerTableView.getColumns().add(vNameCol);
         customerTableView.getColumns().add(nNameCol);
 
         //LISTEN FOR CHANGES (eintrag markiert etc.)
+
         customerTableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Customers>() {
             @Override
             public void changed(ObservableValue<? extends Customers> observableValue, Customers customers, Customers customers2) {
-
             }
         });
-
-        return customerTableView;
     }
 
-        // PRODUCTS TABLE VIEW
-    private TableView createProducts(){
-        final TableView<Products> productsTableView = new TableView();
-
+    private void initProduktTable() {
         productsTableView.setPrefWidth(350);
         productsTableView.setPrefHeight(300);
         productsTableView.setItems(Products.getProducts());
@@ -400,11 +255,39 @@ public class GuiAnwendung extends Application {
                         "Price"));
         priceCol.setPrefWidth(productsTableView.getPrefWidth() / 2);
 
-        // Assemble table from the columns
+        //  Tabelle mit Spalten befuellen
         productsTableView.getColumns().add(NameCol);
         productsTableView.getColumns().add(priceCol);
-
-        return productsTableView;
     }
 
- */
+    /**
+     * returned eine Menubar     *
+     * @return Menubar
+     */
+    private MenuBar createMenu() {
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("File");
+        final Menu menuClose = new Menu("Close");
+        menuBar.getMenus().add(menu);
+        menuBar.getMenus().add(menuClose);
+
+        menu.getItems().add(new MenuItem("New"));
+        menu.getItems().add(new MenuItem("Load"));
+        menu.getItems().add(new MenuItem("Save"));
+        menu.getItems().add(new MenuItem("Properties"));
+        menu.getItems().add(new MenuItem("Print"));
+
+        return menuBar;
+    }
+
+    // laedt die CSS Datei
+    private ObservableList<String> loadCss(String cssFileName) {
+        ObservableList<String> cssStyle = FXCollections.observableArrayList();
+        URL url = getClass().getResource(cssFileName);
+        if (url == null) {
+            return null;
+        }
+        cssStyle.addAll(url.toExternalForm());
+        return cssStyle;
+    }
+}
